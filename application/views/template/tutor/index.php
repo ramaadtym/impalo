7c6d2d1c631b43d004c20e7694728075fe2ad1d6
ramaadtym <ramaadtym@gmail.com>
2017-12-03 08:40:43 +0700

    Perbaikan tabel jadi datatable

--- application/views/template/tutor/index.php
+++ application/views/template/tutor/index.php
@@ -29,12 +29,14 @@
     <link href="<?php echo base_url(); ?>assets/css/style.css" rel="stylesheet">
     <link href="<?php echo base_url(); ?>assets/style.css" rel="stylesheet">
 
+    <link href="<?php echo base_url();?>assets/plugins/jquery-datatable/skin/bootstrap/css/dataTables.bootstrap.css" rel="stylesheet"/>
+
     <!-- AdminBSB Themes. You can choose a theme from css/themes instead of get all themes -->
     <link href="<?php echo base_url(); ?>assets/css/themes/all-themes.css" rel="stylesheet" />
 
 </head>
 <?php if($this->session->userdata('akses') == 'Admin'){?>
-    <body class="theme-blue-grey">
+<body class="theme-blue-grey">
 <?php   } ?>
 <body class="theme-red">
 <?php
@@ -60,32 +62,21 @@ echo $content;
 <!-- Waves Effect Plugin Js -->
 <script src="<?php echo base_url(); ?>assets/plugins/node-waves/waves.js"></script>
 
-<!-- Jquery CountTo Plugin Js -->
-<script src="<?php echo base_url(); ?>assets/plugins/jquery-countto/jquery.countTo.js"></script>
-
-<!-- Morris Plugin Js -->
-<script src="<?php echo base_url(); ?>assets/plugins/raphael/raphael.min.js"></script>
-<script src="<?php echo base_url(); ?>assets/plugins/morrisjs/morris.js"></script>
-
-<!-- ChartJs -->
-<script src="<?php echo base_url(); ?>assets/plugins/chartjs/Chart.bundle.js"></script>
-
-<!-- Flot Charts Plugin Js -->
-<script src="<?php echo base_url(); ?>assets/plugins/flot-charts/jquery.flot.js"></script>
-<script src="<?php echo base_url(); ?>assets/plugins/flot-charts/jquery.flot.resize.js"></script>
-<script src="<?php echo base_url(); ?>assets/plugins/flot-charts/jquery.flot.pie.js"></script>
-<script src="<?php echo base_url(); ?>assets/plugins/flot-charts/jquery.flot.categories.js"></script>
-<script src="<?php echo base_url(); ?>assets/plugins/flot-charts/jquery.flot.time.js"></script>
-
-<!-- Sparkline Chart Plugin Js -->
-<script src="<?php echo base_url(); ?>assets/plugins/jquery-sparkline/jquery.sparkline.js"></script>
-
+<!-- Jquery DataTable Plugin Js -->
+<script src="<?php echo base_url(); ?>assets/plugins/jquery-datatable/jquery.dataTables.js"></script>
+<script src="<?php echo base_url(); ?>assets/plugins/jquery-datatable/skin/bootstrap/js/dataTables.bootstrap.js"></script>
+<script src="<?php echo base_url(); ?>assets/plugins/jquery-datatable/extensions/export/dataTables.buttons.min.js"></script>
+<script src="<?php echo base_url(); ?>assets/plugins/jquery-datatable/extensions/export/buttons.flash.min.js"></script>
+<script src="<?php echo base_url(); ?>assets/plugins/jquery-datatable/extensions/export/jszip.min.js"></script>
+<script src="<?php echo base_url(); ?>assets/plugins/jquery-datatable/extensions/export/pdfmake.min.js"></script>
+<script src="<?php echo base_url(); ?>assets/plugins/jquery-datatable/extensions/export/vfs_fonts.js"></script>
+<script src="<?php echo base_url(); ?>assets/plugins/jquery-datatable/extensions/export/buttons.html5.min.js"></script>
+<script src="<?php echo base_url(); ?>assets/plugins/jquery-datatable/extensions/export/buttons.print.min.js"></script>
 <!-- Custom Js -->
 <script src="<?php echo base_url(); ?>assets/js/admin.js"></script>
-<script src="<?php echo base_url(); ?>assets/js/pages/index.js"></script>
+<script src="<?php echo base_url(); ?>assets/js/pages/tables/jquery-datatable.js"></script>
 
 <!-- Demo Js -->
 <script src="<?php echo base_url(); ?>assets/js/demo.js"></script>
-
 </body>
 </html>
